52cef3b96c942d3073f7b3c13e39dbbb23d6aee8
laxjogamer-ui <laxjogamer@gmail.com>
2026-03-18 22:45:09 -0700

    comment delete
    -

--- pro1/app1/views.py
+++ pro1/app1/views.py
@@ -4,12 +4,12 @@ from .models import VisitingCard
 from .serializers import VisitingCardSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
 
-# Create your views here.
+
 
 
 class VisitingCardViewSet(viewsets.ModelViewSet):
     queryset = VisitingCard.objects.all().order_by('-created_at')
     serializer_class = VisitingCardSerializer
-    parser_classes = (MultiPartParser, FormParser) # ફાઈલ અપલોડ માટે જરૂરી
+    parser_classes = (MultiPartParser, FormParser) 
 
     
